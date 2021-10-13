7bd8ea61633098000badf77cd1bce0143ef1f0b2
Floor20 <austino2112@gmail.com>
2021-10-13 16:32:35 -0600

    Completed design w/JS (adjustment)

--- script.js
+++ script.js
@@ -2,9 +2,11 @@
 
 const arrows = document.querySelectorAll(".arrow");
 const answers = document.querySelectorAll(".faq-answer");
+const questions = document.querySelectorAll(".faq-question");
 
 const answerList = [...answers];
 const arrowList = [...arrows];
+const questionList = [...questions];
 
 // Define starting conditions
 const init = function () {
@@ -13,15 +15,44 @@ const init = function () {
 
 init();
 
+const boldenQuestion = function () {
+  arrowList.forEach((_, index) => {
+    for (const answer of answerList)
+      if (answerList.indexOf(answer) === index)
+        for (const question of questionList)
+          if (
+            answerList.indexOf(answer) === questionList.indexOf(question) &&
+            !answer.classList.contains("hidden")
+          )
+            question.classList.add("bold");
+          else if (
+            answerList.indexOf(answer) === questionList.indexOf(question) &&
+            answer.classList.contains("hidden")
+          )
+            question.classList.remove("bold");
+  });
+};
+
 // Hide and show answers
-arrows.forEach((image) => {
+arrowList.forEach((image) => {
   image.addEventListener("click", function () {
     for (let answer of answerList)
       if (
         answerList.indexOf(answer) === arrowList.indexOf(image) &&
         answer.classList.contains("hidden")
-      )
+      ) {
         answer.classList.remove("hidden");
-      else answer.classList.add("hidden");
+        image.classList.add("rotate");
+
+        boldenQuestion();
+      } else if (
+        answerList.indexOf(answer) === arrowList.indexOf(image) &&
+        !answer.classList.contains("hidden")
+      ) {
+        image.classList.remove("rotate");
+        answer.classList.add("hidden");
+
+        boldenQuestion();
+      }
   });
 });
